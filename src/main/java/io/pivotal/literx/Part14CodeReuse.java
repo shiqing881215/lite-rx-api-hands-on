f78a84609e5270420d614956371e7fff6ae0e106
Julien Hoarau <jhoarau@atlassian.com>
2018-09-17 07:38:26 +1000

    Improve Part14 by having a less contrived example

--- src/main/java/io/pivotal/literx/Part14CodeReuse.java
+++ src/main/java/io/pivotal/literx/Part14CodeReuse.java
@@ -1,8 +1,10 @@
 package io.pivotal.literx;
 
 import io.pivotal.literx.domain.User;
+import io.pivotal.literx.repository.ReactiveUserRepository;
 import java.util.function.Function;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Learn how to reuse operators
@@ -13,13 +15,45 @@ import reactor.core.publisher.Flux;
  */
 public class Part14CodeReuse {
 
-  // TODO Modify the input flux to return more and more users every time
-  // the flux is subscribed
-  // should return 1 user the first time
-  // 2 the second time
-  // 3 the third time...
-  public Flux<User> takeMoreAndMore(Flux<User> users) {
-    return users;
+  private ReactiveUserRepository reactiveUserRepository = new ReactiveUserRepository(100);
+
+  private final MetricRegister metricRegister;
+
+  public Part14CodeReuse(MetricRegister metricRegister) {
+    this.metricRegister = metricRegister;
   }
 
+  // TODO we want to record the `findFirst` duration and add it to MetricRegister
+  public Mono<User> instrumentedFindFirst() {
+    return reactiveUserRepository.findFirst();
+
+  }
+
+  // TODO extract previous logic in a separate function in order to be able
+  // to instrument this stream as well
+  public Mono<User> instrumentedFindUser(String username) {
+    return reactiveUserRepository.findById(username);
+  }
+
+  /**
+   * Poor man stopwatch.
+   * Can be used to record execution time in milliseconds
+   */
+  private static class StopWatch {
+    private long start = -1;
+
+    public void start() {
+      if (start == -1) {
+        start = System.currentTimeMillis();
+      }
+    }
+
+    public long stop() {
+      return start != -1L ? System.currentTimeMillis() - start : -1L;
+    }
+  }
+
+  interface MetricRegister {
+    void timed(String metricName, long durationInMilliseconds);
+  }
 }
--- src/test/java/io/pivotal/literx/Part14CodeReuseTest.java
+++ src/test/java/io/pivotal/literx/Part14CodeReuseTest.java
@@ -1,41 +1,60 @@
 package io.pivotal.literx;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.pivotal.literx.Part14CodeReuse.MetricRegister;
 import io.pivotal.literx.domain.User;
-import io.pivotal.literx.repository.ReactiveUserRepository;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
 import org.junit.Test;
-import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 public class Part14CodeReuseTest {
 
-  Part14CodeReuse workshop = new Part14CodeReuse();
-  ReactiveUserRepository reactiveUserRepository = new ReactiveUserRepository();
+  private final MockMeterRegister metricRegister = new MockMeterRegister();
+  Part14CodeReuse workshop = new Part14CodeReuse(metricRegister);
 
-  @Test
-  public void testTakeMoreAndMore() {
-    final Flux<User> users = reactiveUserRepository.getAll();
-
-    final Flux<User> composed = workshop.takeMoreAndMore(users);
-    StepVerifier.create(composed)
-        .expectNextCount(1)
-        .expectComplete()
-        .verify();
-
-    StepVerifier.create(composed)
-        .expectNextCount(2)
-        .expectComplete()
-        .verify();
-
-    StepVerifier.create(composed)
-        .expectNextCount(3)
-        .expectComplete()
-        .verify();
-
-    StepVerifier.create(composed)
-        .expectNextCount(4)
-        .expectComplete()
-        .verify();
+  @Before
+  public void setUp() throws Exception {
+    metricRegister.reset();
   }
 
+  @Test
+  public void testFindFirst() {
+    StepVerifier.create(workshop.instrumentedFindFirst())
+        .expectNextCount(1)
+        .verifyComplete();
 
+    assertThat(metricRegister.getTime("findFirst")).isGreaterThanOrEqualTo(100);
+  }
+
+  @Test
+  public void testFind() {
+    StepVerifier.create(workshop.instrumentedFindUser(User.SKYLER.getUsername())
+        .repeat(2))
+        .expectNextCount(2)
+        .verifyComplete();
+
+    assertThat(metricRegister.getTime("findUser")).isGreaterThanOrEqualTo(100);
+    assertThat(metricRegister.getTime("findUser")).isLessThan(200);
+  }
+
+  private static class MockMeterRegister implements MetricRegister {
+
+    private final Map<String, Long> timeMetrics = new HashMap<>();
+
+    @Override
+    public void timed(String metricName, long durationInMilliseconds) {
+      timeMetrics.put(metricName, durationInMilliseconds);
+    }
+
+    public long getTime(String metricName) {
+      return timeMetrics.getOrDefault(metricName, -1L);
+    }
+
+    public void reset() {
+      timeMetrics.clear();
+    }
+  }
 }
